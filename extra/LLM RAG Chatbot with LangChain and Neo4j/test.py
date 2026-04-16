3bc90343f78d1b782374c1832cf8d01ad66d7139
Quan Nguyen <nguyenhoangdangquan2804@gmail.com>
2026-04-16 22:50:33 +0700

    Update wait_times function

--- extra/LLM RAG Chatbot with LangChain and Neo4j/test.py
+++ extra/LLM RAG Chatbot with LangChain and Neo4j/test.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 # from src.chains.hospital_review_chain import reviews_vector_chain
-from src.chains.hospital_cypher_chain import hospital_cypher_chain
+# from src.chains.hospital_cypher_chain import hospital_cypher_chain
 
 # query = """
 #         What have patients said about hospital efficiency?
@@ -14,7 +14,16 @@ from src.chains.hospital_cypher_chain import hospital_cypher_chain
 # print(response.get("result"))
 
 # question = """What is the average visit duration for emergency visits in North Carolina?"""
-question="""Which region have the most visit?"""
-response = hospital_cypher_chain.invoke(question)
+# question="""What is the average salary of physician?"""
+# response = hospital_cypher_chain.invoke(question)
 
-print(response)
+# print(response)
+
+from src.tools.wait_times import get_current_wait_times,get_most_available_hospital
+
+print(get_current_wait_times("Wallace-Hamilton"))
+
+print(get_current_wait_times("fake hospital"))
+
+print(get_most_available_hospital(None))
+
